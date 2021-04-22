b104fa194e5fcf109d78462a808c67637655d9a5
Suresh Soundararajan <suresh.soundararajan1@gmail.com>
2021-04-21 22:42:01 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>My Portfolio</h1>
+      <h2>Coming Soon!</h2>
+    </div>
+  );
+}
+
+export default App;
