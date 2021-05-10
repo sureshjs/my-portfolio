88c225e742cc3433df48048a2e3fbc57203d89db
Suresh Soundararajan <suresh.soundararajan1@gmail.com>
2021-05-10 02:19:17 -0500

    nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Projects from "./components/Projects";
+import Articles from "./components/Articles";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className='App'>
-      <h1>My Portfolio</h1>
-      <h2>Coming Soon!</h2>
-      <h3>CI Test</h3>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Route exact path='/' component={Projects} />
+        <Route path='/articles' component={Articles} />
+        <Route path='/about' component={About} />
+        <h1>My Portfolio</h1>
+        <h2>Coming Soon!</h2>
+        <h3>CI Test</h3>
+        <nav>
+          <img src={logo} className='logo' alt='Logo Image' />
+          <div className='navigation-sub'>
+            <Link to='/' className='item'>
+              Projects
+            </Link>
+            <Link href='/articles' className='item'>
+              Articles
+            </Link>
+            <Link href='/about' className='item'>
+              About
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </BrowserRouter>
   );
 }
 
